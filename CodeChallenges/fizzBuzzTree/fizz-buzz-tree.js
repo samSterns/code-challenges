0233610fc247677c92d3a7687918db68f5cb20da
samSterns <sam.g.sterns@gmail.com>
2020-02-04 21:07:23 -0800

    logic finished in pseudo

--- CodeChallenges/fizzBuzzTree/fizz-buzz-tree.js
+++ CodeChallenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -4,8 +4,13 @@ const { BinarySearchTree, BinaryTree } = require('../Tree/tree');
 const fizzBuzzTree = tree => {
     const binaryTree = new BinaryTree(tree);
 
-// map over values - conditional statement if remainder 3 is 0 then fizz 
+// map over values - conditional statement:
+// If the value is divisible by 3, replace the value with “Fizz”
+// If the value is divisible by 5, replace the value with “Buzz”
+// If the value is divisible by 3 and 5, replace the value with “FizzBuzz”
+// If the value is not divisible by 3 or 5, simply turn the number into a String. 
 //if '' 5 is buzz
+
 // return a new binary search
 
 };
