d8961e93d5ffc6a2c762a1f0036e4f21cbda8f79
samSterns <sam.g.sterns@gmail.com>
2020-02-07 17:18:12 -0800

    file set up

---
+++ find-maximum-binary-tree/find-maximum-binary-tree.js
@@ -0,0 +1,82 @@
+// copied from Tree.js to preserve that file
+
+Class Node {
+    constructor(value, left = null, right = null) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BinaryTree {
+    constructor(tree) {
+        this.root = tree.root;
+    }
+    preOrder(){
+        if(!this.root) return;
+        const array = [];
+        (function castPreArray(current){
+          if(current){
+            array.push(current.value);
+            castPreArray(current.left);
+            castPreArray(current.right);
+          }
+        }).apply(null, [this.root]);
+        return array;
+    }
+    inOrder(){
+        if(!this.root) return;
+        const array = [];
+        (function castInArray(current){
+          if(current){
+            castInArray(current.left);
+            array.push(current.value);
+            castInArray(current.right);
+          }
+        }).apply(null, [this.root]);
+        return array;
+    }
+
+    breadthFirst(){
+        if(!this.root) return;
+        const queue = [this.root];
+        const results = [];
+
+        while(queue.length > 0) {
+        const current = queue[0];
+        if(current.left) queue.push(current.left);
+        if(current.right) queue.push(current.right);
+        results.push(queue.shift().value);
+        }
+        return results;
+    }
+
+  breadthFirst() {
+      if(!this.root) return;
+      const queue = [this.root];
+      const results = [];
+
+      while(queue.length > 0) {
+          const current = queue[0];
+          if(current.left) queue.push(current.left);
+          if(current.right) queue.push(current.right);
+          results.push(queue.shift().value)
+      }
+      return results; 
+  }
+
+  findMax() {
+    if(!this.root) return;
+    let max = this.root.value;
+
+    (function castPreArray(current){
+      if(current){
+        max = Math.max(current.value, max);
+        castPreArray(current.left);
+        castPreArray(current.right);
+      }
+    }).apply(null, [this.root]);
+    return max;
+  }
+}
+
